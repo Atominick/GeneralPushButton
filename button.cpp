39cc92a37038b6c326888b1e8991bc426c634e9c
Atominick <vova.gerish@gmail.com>
2021-09-06 16:51:07 +0300

    code added)

---
+++ button.cpp
@@ -0,0 +1,93 @@
+#include "button.h"
+
+
+Button::Button(volatile uint32_t *registerToCheck, const uint32_t registerMask, const uint8_t stateWhenOn)
+    : register_to_check(registerToCheck), register_mask(registerMask), state_when_on(stateWhenOn) {}
+
+void Button::tick() {
+    if(!(*register_to_check & register_mask) ^ state_when_on) { // Pushed
+        if(!freezed) {
+            pressed_time++;
+
+            if(pressed_time == CLICK_DELAY) {
+                state = CLICKED;
+                if(released_time > MULTIPLE_CLICKING_DELAY) {
+                    clicked_counter = 0;
+                }
+            } else if(pressed_time == PRESS_DELAY) {
+                state = PRESSED;
+            } else if(pressed_time == LONG_PRESS_DELAY) {
+                state = LONG_PRESSED;
+            } else if(pressed_time > TIMEOUT_DELAY) {
+                state = UNCERTAIN; // TimeOut
+            }
+
+            if(pressed_time > CLAMPED_DELAY) {
+                mods = (Mod)((int)CLAMPED | (int)mods);
+                clamped_counter++;
+            }
+        }
+    } else { // Released
+        released_time++;
+
+        if(freezed) {
+            reset();
+            freezed = 0;
+        }
+
+        if(pressed_time) {
+            state = UNCERTAIN; // For values smaller then minimum
+
+            if(pressed_time > LONG_PRESS_DELAY) {
+                state = LONG_PRESS_RELEASED;
+            } else if(pressed_time > PRESS_DELAY) {
+                state = PRESS_RELEASED;
+            } else if(pressed_time > CLICK_DELAY) {
+                state = CLICK_RELEASED;
+            }
+
+            // Multiple clicking
+            if(state == CLICK_RELEASED) {
+                // debug_printf("CC: %d\n", clicked_counter);
+                switch(++clicked_counter) {
+                case 2:
+                    mods = (Mod)((int)DOUBLE_CLICKED | (int)mods);
+                    break;
+                case 3:
+                    mods = (Mod)((int)TRIPLE_CLICKED | (int)mods);
+                    break;
+                }
+
+            } else {
+                clicked_counter = 0;
+            }
+
+            clamped_counter = 0;
+            released_time = 0;
+            pressed_time = 0;
+        }
+    }
+}
+
+uint8_t Button::isMod(Mod mod_to_check) {
+    if((int)mods & (int)mod_to_check)
+        return 1;
+    else
+        return 0;
+}
+
+uint8_t Button::clearState() {
+    state = State::NO_STATE;
+    mods = Mod::NO_MODS;
+}
+
+void Button::reset() {
+    pressed_time = 0;
+    released_time = 0;
+    clamped_counter = 0;
+    clearState();
+}
+
+void Button::freeze() {
+    freezed = 1;
+}
---
+++ button.h
@@ -0,0 +1,76 @@
+#ifndef BUTTON_H_
+#define BUTTON_H_
+
+#include <stdint.h>
+
+
+#ifndef BUTTON_TICKING_FREQUENCY
+#define BUTTON_TICKING_FREQUENCY 100    // in Hz
+#endif
+
+#define TO_TICKS(s) int(s * (1000 / BUTTON_TICKING_FREQUENCY))
+
+// Delays in ms
+#define CLICK_DELAY TO_TICKS(60)    // Must exceed debounce time
+#define PRESS_DELAY TO_TICKS(500)
+#define CLAMPED_DELAY TO_TICKS(2000)
+#define LONG_PRESS_DELAY TO_TICKS(2010)
+#define MULTIPLE_CLICKING_DELAY TO_TICKS(150)
+#define TIMEOUT_DELAY TO_TICKS(10000)
+
+
+
+class Button
+{
+public:
+    enum State
+    {
+        NO_STATE = 0x00,
+        CLICKED,
+        PRESSED,
+        LONG_PRESSED,
+
+        CLICK_RELEASED,
+        PRESS_RELEASED,
+        LONG_PRESS_RELEASED,
+
+        UNCERTAIN,
+    };
+
+    enum Mod
+    {
+        NO_MODS = 0x00,
+        CLAMPED = 0x01,
+        DOUBLE_CLICKED = 0x02,
+        TRIPLE_CLICKED = 0x04
+    };
+
+public:
+    Button(volatile uint32_t *registerToCheck, const uint32_t registerMask, const uint8_t stateWhenOn = 1);
+
+    void reset();
+    void freeze();
+    uint8_t isMod(Mod mod_to_check);
+    uint8_t clearState();
+
+    void tick();
+
+private:
+    volatile uint32_t *register_to_check = nullptr;
+    const uint32_t register_mask;
+    const uint8_t state_when_on;
+
+    uint8_t clicked_counter = 0;
+    uint32_t released_time = 0;
+    uint32_t pressed_time = 0;
+    uint8_t freezed = 0;
+
+    // For clamping proportional with time
+    uint32_t clamped_counter = 0;
+    volatile Mod mods = Mod::NO_MODS;
+
+public:
+    volatile State state = State::NO_STATE;
+};
+
+#endif // BUTTON_H_
